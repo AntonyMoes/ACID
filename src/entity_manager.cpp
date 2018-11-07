5fd2e77ed7eec04f8b1b889387a226ef815e6e45
Vladimir <ukhachev.v.s@gmail.com>
2018-11-07 21:03:51 +0300

    implement managers

---
+++ src/entity_manager.cpp
@@ -0,0 +1,21 @@
+#include <entity_manager.h>
+
+EntityManager::EntityManager(NodeManager* _node_manager): node_manager(_node_manager) {
+
+}
+
+void EntityManager::add_entity(Entity *entity) {
+	auto id = entity->get_id();
+	entity_map[id] = entity;
+	node_manager->add_nodes(entity);
+}
+
+Entity* EntityManager::get_entity(size_t id) {
+	return entity_map[id];
+}
+
+void EntityManager::delete_entity(size_t id) {
+	delete entity_map[id];
+	entity_map->erase(id);
+}
+
---
+++ src/node_manager.cpp
@@ -0,0 +1,16 @@
+#include <node_manager.h>
+#include <iterator>
+
+void NodeManager::add_prototype(INode* node) {
+	node_prototypes.push_back(node);
+}
+
+void NodeManager::add_nodes(Entity* entity) {
+	auto components = entity->get_components();
+	for (auto i = node_prototypes.begin(); i != node_prototypes.end(); ++i) {
+		if (i->initialize_components(components)) {
+			entity->add_node(i->clone());
+		}
+	}
+}
+
