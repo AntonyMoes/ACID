ce053d9182c60802aa8775c344b8c8ddca58d8aa
indiagolph99 <savva.gorodetsky@gmail.com>
2018-11-10 21:05:04 +0300

    Gtest added in local dir

---
+++ include/test/test.h
@@ -0,0 +1,20 @@
+//
+// Created by savva on 10.11.18.
+//
+
+#ifndef ACID_TEST_TEST_H
+#define ACID_TEST_TEST_H
+#include <gtest/gtest.h>
+#include <node.h>
+#include <active_system.h>
+#include <i_component.h>
+
+#include <entity.h>
+#include <game_loop.h>
+
+#include <typeindex>
+#include <iostream>
+ class SetupTestComponent : public ::testing::Test {
+
+};
+#endif //ACID_TEST_H
---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by savva on 10.11.18.
+//
+
+#include "test.h"
