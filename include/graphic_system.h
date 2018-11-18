850e11a26db4ebed30dac9ac3f96912e308e4192
Xenobyte42 <andmicha2011@bk.ru>
2018-11-18 18:17:47 +0300

    fix graph_system

--- include/graphic_system.h
+++ include/graphic_system.h
@@ -17,7 +17,12 @@ class GraphicalSystem: public ActiveSystem<CameraNode> {
     ~GraphicalSystem() = default;
 
     void execute() const override {
-        drawable_objects = camera->get_scope();
+        auto cam_node = active_nodes.front();
+        auto cam_coord_comp = cam_node->get_component<PositionComponent>();
+        sf::Vector2f center = cam_coord_comp->get_coords();
+        sf::Vector2u window_size = window->getSize();
+
+        drawable_objects = camera->get_scope(center, window_size.x, window_size.y);
         for (const auto& obj: drawable_objects) {
             sf::Vector2f pos = obj.first;
             sf::Sprite sprite = obj.second->get_sprite();
--- src/graph_test.cpp
+++ src/graph_test.cpp
@@ -1,6 +1,13 @@
 #include <game_loop.h>
+#include <graphic_system.h>
+#include <input_system.h>
+#include <camera_system.h>
+#include <displayer_system.h>
+
 
 int main() {
+    sf::RenderWindow window;
+    GameLoop gameloop;
 
     return 0;
 }
