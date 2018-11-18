8a3b1f554127a7a11207b3c43f045840d8998f4e
Xenobyte42 <andmicha2011@bk.ru>
2018-11-18 16:00:50 +0300

    Camera component and camera node

---
+++ include/camera_node.h
@@ -0,0 +1,17 @@
+#ifndef ACID_INCLUDE_CAMERA_NODE_H_
+#define ACID_INCLUDE_CAMERA_NODE_H_
+
+#include <node.h>
+#include <coordinate_component.h>
+#include <camera_component.h>
+
+class CameraNode: public Node<CameraNode> {
+  public:
+    CameraNode() {
+        add_component<CameraComponent>();
+        add_component<CoordinateComponent>();
+    }
+};
+
+
+#endif  // ACID_INCLUDE_CAMERA_NODE_H_
