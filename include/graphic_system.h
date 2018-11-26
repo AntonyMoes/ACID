12fac9593d63bebcdf27f928cdd9c96743c95dc7
Xenobyte42 <andmicha2011@bk.ru>
2018-11-26 23:33:37 +0300

    Graph system now drawing correct center object

--- include/graphic_system.h
+++ include/graphic_system.h
@@ -14,28 +14,33 @@ class GraphicSystem: public ActiveSystem<CameraNode> {
     window(window),
     camera(camera) {}
 
-    ~GraphicSystem() = default;
-
     void execute() const override {
         if (active_nodes.empty()) {
             return;
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape))
-            {
-                window->close();
-            }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
+            window->close();
+        }
         auto cam_node = *active_nodes.begin();
         auto cam_coord_comp = cam_node->get_component<PositionComponent>();
         sf::Vector2f center = cam_coord_comp->get_coords();
+        std::cout << "x: " << center.x << " y: " << center.y << std::endl;
+
         sf::Vector2u window_size = window->getSize();
+        sf::Vector2f window_center(static_cast<float>(window_size.x) / 2,
+                                   static_cast<float>(window_size.y) / 2);
 
         auto drawable_objects = camera->get_scope(center, window_size.x, window_size.y);
+        auto view = window->getView();
+        view.setCenter(center.x + window_center.x, center.y + window_center.y);
+        window->setView(view);
         for (const auto& obj: drawable_objects) {
             sf::Vector2f pos = obj.first;
-            std::cout << "x: " << pos.x << " y: " << pos.y << std::endl;
             sf::Sprite sprite = obj.second->get_component<TextureComponent>()->get_sprite();
             sprite.setPosition(pos);
-            sprite.move(pos);
+            sf::Vector2f offset(pos.x - center.x, pos.y - center.y);
+
+            sprite.move(window_center.x + offset.x, window_center.y + offset.y);
             window->draw(sprite);
         }
     }
