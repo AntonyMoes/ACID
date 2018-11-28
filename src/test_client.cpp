b086c540f20a20b5516dcef2876c4e0d9b64d2d4
Vladimir <ukhachev.v.s@gmail.com>
2018-11-28 23:48:26 +0300

    network systems

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -1,3 +1,35 @@
+#include <framework/i_component.h>
+#include <node.h>
+#include <entity_life_system.h>
+#include <framework/game_loop.h>
+#include <network_manager.h>
+
+class PlayerComponent: public IComponent {
+  public:
+    std::string& get_nick();
+    void set_nick(const std::string& _nick);
+  private:
+    std::string nick;
+};
+
+class PlayerNode : public Node <PlayerNode> {
+    PlayerNode() {
+        add_component<PlayerComponent>();
+    }
+};
+
+class NetworkSpawnSystem : public EntityLifeSystem {
+    pu
+    NetworkSpawnSystem(NetworkManager* _net) {
+
+    }
+
+};
+
 int main() {
+    GameLoop loop;
+    NetworkSpawnSystem spawn_system;
+    loop.register_life_system(&spawn_system);
+
     return 0;
 }
