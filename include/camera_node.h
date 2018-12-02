a3dde1448602a80c69b691d13084cfb26e924b64
AntonyMo <32247865+antonymoes@users.noreply.github.com>
2018-12-02 16:31:33 +0300

    moving fxd

--- include/camera_node.h
+++ include/camera_node.h
@@ -2,14 +2,14 @@
 #define ACID_INCLUDE_CAMERA_NODE_H_
 
 #include <node.h>
-#include <position_component.h>
+#include <collision_component.h>
 #include <camera_component.h>
 
 class CameraNode: public Node<CameraNode> {
   public:
     CameraNode() {
         add_component<CameraComponent>();
-        add_component<PositionComponent>();
+        add_component<CollisionComponent>();
     }
 };
 
--- include/move_system.h
+++ include/move_system.h
@@ -6,27 +6,38 @@
 #include <move_node.h>
 
 
-class MoveSystem: public ReactiveSystem<MoveNode> {
+class MoveSystem: public ActiveSystem<MoveNode> {
   public:
     void execute() override {
-        for (auto& node : reactive_nodes) {
+        for (auto& node : active_nodes) {
             auto input_move_comp = node->get_component<InputMoveComponent>();
             auto col_comp = node->get_component<CollisionComponent>();
 
             auto keys = input_move_comp->get_keys();
             auto body = col_comp->get_body();
+
+            b2Vec2 speed{0, 0};
+
             if ((*keys)[sf::Keyboard::W]) {
-                body->SetLinearVelocity(b2Vec2(0.0f, -2.0f));
+                //body->SetLinearVelocity(b2Vec2(0.0f, -20.0f));
+                speed.y -= 20;
             }
             if ((*keys)[sf::Keyboard::A]) {
-                body->SetLinearVelocity(b2Vec2(-2.0f, 0.0f));
+                //body->SetLinearVelocity(b2Vec2(-20.0f, 0.0f));
+                speed.x -= 20;
             }
             if ((*keys)[sf::Keyboard::S]) {
-                body->SetLinearVelocity(b2Vec2(0.0f, 2.0f));
+                //body->SetLinearVelocity(b2Vec2(0.0f, 20.0f));
+                speed.y += 20;
             }
             if ((*keys)[sf::Keyboard::D]) {
-                body->SetLinearVelocity(b2Vec2(2.0f, 0.0f));
+                //body->SetLinearVelocity(b2Vec2(20.0f, 0.0f));
+                speed.x += 20;
             }
+
+            body->SetLinearVelocity(speed);
+
+
         }
     }
 };
