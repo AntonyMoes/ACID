b4523810067467b12ae110f4ea699c38f2507578
AntonyMo <32247865+antonymoes@users.noreply.github.com>
2018-12-02 21:19:45 +0300

    local merge conflict fxd

---
+++ include/input_mouse_system.h
@@ -0,0 +1,14 @@
+#ifndef ACID_INCLUDE_INPUT_MOUSE_SYSTEM_H_
+#define ACID_INCLUDE_INPUT_MOUSE_SYSTEM_H_
+
+
+#include <framework/active_system.h>
+
+class InputMouseSystem : public ActiveSystem<InputMouseNode> {
+  public:
+    void execute()  {
+
+    }
+};
+
+#endif  // ACID_INCLUDE_INPUT_MOUSE_SYSTEM_H_
