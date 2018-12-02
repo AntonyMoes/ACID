634c3b98198484e059a7707aa6a2c4355362f51e
Xenobyte42 <andmicha2011@bk.ru>
2018-12-02 18:49:22 +0300

    Add base entity

---
+++ include/fireball.h
@@ -0,0 +1,11 @@
+#ifndef ACID_INCLUDE_FIREBALL_H_
+#define ACID_INCLUDE_FIREBALL_H_
+
+
+#include <entity.h>
+
+class Fireball: public Entity {
+    Fireball(): Entity() {}
+};
+
+#endif  // ACID_INCLUDE_FIREBALL_H_
