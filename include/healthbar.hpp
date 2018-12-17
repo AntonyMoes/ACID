64bd72494f61901063602cb8436dd92d7520e9fa
indiagolph99 <savva.gorodetsky@gmail.com>
2018-12-17 04:46:52 +0300

    SP bar dummy added

--- include/healthbar.hpp
+++ include/healthbar.hpp
@@ -18,21 +18,30 @@ class Healthbar : public ActiveSystem<HPNode> {
 
     void execute() override {
         sf::Texture htx;
-            sf::Sprite health;
-            auto node = *active_nodes.begin();
-            if(node != nullptr) {
-                auto *player = node->get_component<HealthComponent>();
-                auto hp = player->get_hp();
-                auto max_hp = player->get_max_hp();
+        sf::Sprite health;
+        sf::Sprite mana;
+        auto node = *active_nodes.begin();
+        if(node != nullptr) {
+                auto *h_comp = node->get_component<HealthComponent>();
+                auto hp = h_comp->get_hp();
+                auto max_hp = h_comp->get_max_hp();
                 health.setTexture(*tm->getHP(hp, max_hp));
+                auto *m_comp = node->get_component<ManaComponent>();
+                auto sp = m_comp->get_mana();
+                auto max_sp = m_comp->get_max_mana();
+                mana.setTexture(*tm->getHP(sp, max_sp));
+
         }
         health.setScale(sf::Vector2f(0.5f, 0.5f));
-        auto screen = window->getSize();
+        mana.setScale(sf::Vector2f(0.5f, 0.5f));
 
+        auto screen = window->getSize();
+        mana.setPosition(sf::Vector2f(0, 50));
         sf::View hpView(sf::Vector2f(screen.x / 2, screen.y / 2), sf::Vector2f(screen.x, screen.y));
         sf::View defaultView = window->getView();
         window->setView(hpView);
         window->draw(health);
+        window->draw(mana);
         window->setView(defaultView);
     }
 
--- include/hp_node.hpp
+++ include/hp_node.hpp
@@ -6,14 +6,14 @@
 
 #include <node.h>
 #include <health_component.h>
+#include <mana/mana_component.h>
 #include <camera_component.h>
-
-
 class HPNode: public Node<HPNode> {
  public:
     HPNode() {
         add_component<CameraComponent>();
         add_component<HealthComponent>();
+        add_component<ManaComponent>();
     }
 };
 #define ACID_HP_NODE_HPP
